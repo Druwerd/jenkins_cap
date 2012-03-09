1ad048288ee54c490cb62a55ae537bb59e98ffdf
Dru Ibarra <Druwerd@gmail.com>
2012-03-08 17:42:39 -0800

    adding unit test

--- test/test_jenkins_cap.rb
+++ test/test_jenkins_cap.rb
@@ -11,6 +11,8 @@ class TestJenkinsCap < Test::Unit::TestCase
     @config.set :current_revision, "1"
   end
   
+  # Unit tests for helper functions
+  #
   should "get a successful JSON response" do
     assert @config.jenkins.json_request("#{@config.jenkins_host}/api/json")
   end
@@ -18,4 +20,11 @@ class TestJenkinsCap < Test::Unit::TestCase
   should "get an Array of build numbers" do
      assert_kind_of Array, @config.jenkins.get_builds
   end
+  
+  should "get a revision number (or nil) without error" do
+     # no assertion here because can't be sure what we'll get from Jenkins
+     @config.jenkins.get_revision_from_build("1")
+  end
+  
+  # Unit tests for Capistrano tasks
 end
